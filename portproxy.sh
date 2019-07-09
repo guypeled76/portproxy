d5eb9f7eee0a673d570c76bc0b63c8bd4cd3e793
Guy Peled <guy.p@taboola.com>
2019-07-09 11:48:54 +0300

    create port proxy

---
+++ portproxy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+go run portproxy.go data/local.properties data/remote.properties
--- proxyport.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-go run proxyport.go data/local.properties data/remote.properties
